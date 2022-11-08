09fad868aaeff4a0fff7f796f905bec3fe861045
root <Abhinav640>
2022-11-08 19:46:05 +0000

    maven first code

---
+++ src/main/java/com/abhinav/app/App.java
@@ -0,0 +1,13 @@
+package com.abhinav.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
